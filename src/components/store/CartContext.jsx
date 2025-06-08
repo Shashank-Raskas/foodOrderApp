af154faa2a6f88e576a38f41ad04a5b90e222084
Shashank-Raskas <shashankrk20@gmail.com>
2025-06-08 22:28:35 +0530

    used context providers to stop props drilling and reducer also for dynamically updating the cart quantity

--- src/components/store/CartContext.jsx
+++ src/components/store/CartContext.jsx
@@ -9,9 +9,9 @@ const CartContext = createContext({
 function cartReducer(state, action) {
     if (action.type === 'ADD_ITEM') {
         //state.items.push(action.item); //! if this is used , and single item clicke dmany times, it will be added multiple times
-        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id); //? find the index of the item to be added in the cart
 
-        const updatedItems = [...state.items]; // create a copy of the items array
+        const updatedItems = [...state.items]; // create a copy of the items array to avoid mutating the state directly
         if (existingCartItemIndex > -1) {  //!if the item is already in the cart, we need to update the quantity
             const existingItem = state.items[existingCartItemIndex];
             const updatedItem = {
@@ -20,23 +20,60 @@ function cartReducer(state, action) {
             }
             updatedItems[existingCartItemIndex] = updatedItem; // update the item in the array
         } else {
-            updatedItems.push({...action.item,quantity : 1}); // add the new item to the array with quantity 1
+            updatedItems.push({ ...action.item, quantity: 1 }); // add the new item to the array with quantity 1
         }
 
-        return { ...state,
+        return {
+            ...state,
             items: updatedItems,
         };
     }
     if (action.type === 'REMOVE_ITEM') {
-        //remove the item from the state
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id); //? find the index of the item to be removed in the cart
+
+        const existingCartItem = state.items[existingCartItemIndex]; //? get the item to be removed from the cart
+
+        const updatedItems = [...state.items]; // create a copy of the items array to avoid mutating the state directly
+
+        if (existingCartItem.quantity === 1) {
+            updatedItems.splice(existingCartItemIndex, 1); // remove the item from the array
+        }
+        else {
+            updatedItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity - 1,
+            }
+            updatedItems[existingCartItemIndex] = updatedItem; // update the item in the array
+        }
+        return {
+            ...state,
+            items: updatedItems,
+        };
     }
     return state;
 }
 
 export function CartContextProvider({ children }) {
 
-    useReducer(cartReducer, { items: [] });
-    return <CartContext>{children}</CartContext>
+    const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+    
+    function addItem(item) {
+        dispatchCartAction({ type: 'ADD_ITEM', item: item });
+        
+    }
+    function removeItem(id){
+        dispatchCartAction({ type: 'REMOVE_ITEM', item: id });
+    }
+
+    const cartContext = {
+        items: cart.items,
+        addItem,
+        removeItem
+    };
+
+    console.log(cartContext);
+    return <CartContext value={cartContext}>{children}</CartContext>
 }
 
 export default CartContext;
