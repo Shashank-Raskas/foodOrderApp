df9b16bcff5931e84780a09bbd14de27009ce8bf
Shashank-Raskas <shashankrk20@gmail.com>
2025-06-18 16:23:30 +0530

    implemented the new modal opening via new context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import { useContext } from 'react';
 import logo from '../assets/logo.jpg';
 import Button from './UI/Button';
 import CartContext from './store/CartContext';
+import UserProgressContext from './store/UserProgressContext';
 
 export default function Header() {
     const cartCtx = useContext(CartContext);
+    const userProgresCtx = useContext(UserProgressContext);
 
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return totalNumberOfItems + item.quantity;
     }, 0); // Calculate the total number of items in the cart
+
+    function handleShowCart() {
+        userProgresCtx.showCart(); // Show the cart when the button is clicked
+    }
 
     return <header id="main-header">
         <div id="title">
@@ -16,7 +22,7 @@ export default function Header() {
             <h1>ReactFood</h1>
         </div>
         <nav>
-            <Button textOnly>Cart ({totalCartItems})</Button>
+            <Button onClick={handleShowCart} textOnly>Cart ({totalCartItems})</Button>
         </nav>
     </header>
     
---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+export default function  Modal({children,open,className = ''}) {
+
+    const dialog = useRef();
+    useEffect(() => {
+        const Modal = dialog.current;
+        if (open) {
+            dialog.current.showModal();
+        }
+        return () => Modal.close();
+    }, [open]);
+
+    return createPortal(
+    <dialog ref={dialog} className={`modal ${className}`}>{children}</dialog>,
+     document.getElementById('modal')
+    );
+}
