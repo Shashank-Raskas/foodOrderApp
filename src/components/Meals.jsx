0ebe36d4a00afef8e782756f7c2558a266b26ea8
Shashank-Raskas <shashankrk20@gmail.com>
2025-06-20 20:10:18 +0530

    created custom hook useHttp to avoid common error states

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,25 +1,49 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp";
 
+const requestConfig ={};
 
 export default function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
 
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch("http://localhost:3000/meals");
-            if (!response.ok) {
-                ///....
-            }
+   const {
+    data: loadedMeals,
+    isLoading,
+    error,
+   } =  useHttp("http://localhost:3000/meals", requestConfig , []);
+
+    // if error occurs, we can show it to the user
+    if (isLoading) {
+        return <p>Fetching Meals...</p>;
+    }
+
+
+
+   //!code is commented cuz we are using useHttp hook to fetch data
+    // const [loadedMeals, setLoadedMeals] = useState([]);
+
+    // useEffect(() => {
+    //     async function fetchMeals() {
+    //         const response = await fetch("http://localhost:3000/meals");
+    //         if (!response.ok) {
+    //             ///....
+    //         }
     
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-        fetchMeals();
-    },[]);
+    //         const meals = await response.json();
+    //         setLoadedMeals(meals);
+    //     }
+    //     fetchMeals();
+    // },[]);
 
     // if we call fetchMeals here, it will be called on every render, so we need to use useEffect
+
+
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    console.log(loadedMeals," loadedMeals");
     return( 
     <ul id="meals">
         {loadedMeals.map((meal) => (
